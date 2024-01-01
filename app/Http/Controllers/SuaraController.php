606547b2674d5696acecb151b1f8236647e3a033
bagusuntoro <untorobagus65@gmail.com>
2024-01-01 18:48:43 +0700

    fix suara

--- app/Http/Controllers/SuaraController.php
+++ app/Http/Controllers/SuaraController.php
@@ -38,21 +38,84 @@ public function listSuara()
 
 
 
+// public function inputSuara(Request $request)
+// {
+//     $validator = Validator::make($request->all(), [
+//         '*.jumlahSuara' => 'numeric|required_with:*.tpsId,*.kandidatId',
+//         '*.tpsId' => 'numeric|required_with:*.jumlahSuara,*.kandidatId',
+//         '*.kandidatId' => 'numeric|required_with:*.jumlahSuara,*.tpsId',
+//         '*.bukti_suara' => 'required_without:*.jumlahSuara,*.tpsId,*.kandidatId|image|mimes:jpeg,png,jpg,gif|max:2048'
+//     ]);
+
+//     if ($validator->fails()) {
+//         return response()->json(['message' => 'Validasi gagal', 'errors' => $validator->errors()], 400);
+//     }
+
+//     return response()->json(['requestData' => $request->all()], 200);
+// }
+
 public function inputSuara(Request $request)
 {
-    $validator = Validator::make($request->all(), [
-        '*.jumlahSuara' => 'numeric|required_with:*.tpsId,*.kandidatId',
-        '*.tpsId' => 'numeric|required_with:*.jumlahSuara,*.kandidatId',
-        '*.kandidatId' => 'numeric|required_with:*.jumlahSuara,*.tpsId',
-        '*.bukti_suara' => 'required_without:*.jumlahSuara,*.tpsId,*.kandidatId|image|mimes:jpeg,png,jpg,gif|max:2048'
-    ]);
+    // $request->validate([
+    //     '*.jumlahSuara' => 'numeric|required_with:*.tpsId,*.kandidatId',
+    //     '*.tpsId' => 'numeric|required_with:*.jumlahSuara,*.kandidatId',
+    //     '*.kandidatId' => 'numeric|required_with:*.jumlahSuara,*.tpsId',
+    //     '*.bukti_suara' => 'required_without_all:jumlahSuara,tpsId,kandidatId|string'
+    // ]);
 
-    if ($validator->fails()) {
-        return response()->json(['message' => 'Validasi gagal', 'errors' => $validator->errors()], 400);
+    try {
+        $idTps = '0';
+        $buktiSuaraBase64 = null; // Inisialisasi variabel untuk menyimpan base64 terakhir
+
+        foreach ($request->input('data') as $input) {
+            // Periksa apakah objek memiliki bukti_suara
+            if (isset($input['bukti_suara'])) {
+                $buktiSuaraBase64 = $input['bukti_suara'];
+            } else {
+                if (!isset($input['jumlahSuara'], $input['tpsId'], $input['kandidatId'])) {
+                    return response()->json(['message' => 'Input tidak valid. Setiap objek harus memiliki jumlahSuara, tpsId, dan kandidatId.'], 400);
+                }
+
+                $jumlahSuara = $input['jumlahSuara'];
+                $tpsId = $input['tpsId'];
+                $idTps = $tpsId;
+                $kandidatId = $input['kandidatId'];
+
+                // cek apakah tps sudah diisi
+                $status = TPS::where('id', $idTps)->value('status');
+                if ($status == '0') {
+                    Suara::create(['jumlahSuara' => $jumlahSuara, 'tpsId' => $tpsId, 'kandidatId' => $kandidatId]);
+                } else {
+                    return response()->json(['message' => 'Pengisian suara gagal, Suara di tps ini sudah diisi sebelumnya'], 400);
+                }
+            }
+        }
+
+        // Setelah loop selesai, simpan base64 terakhir ke objek TPS jika ada
+        if ($buktiSuaraBase64) {
+            // Decode Base64 dan simpan sebagai file
+            $buktiSuaraBinary = base64_decode($buktiSuaraBase64);
+            $imageName = uniqid() . '.png'; // Nama file unik
+            $path = storage_path('app/public/fileBukti/' . $imageName);
+
+            file_put_contents($path, $buktiSuaraBinary);
+
+            // Update kolom bukti_suara pada TPS
+            $tps = TPS::find($idTps);
+            $tps->bukti_suara = $imageName;
+            $tps->save();
+        }
+
+        TPS::where('id', $idTps)->update(['status' => '1']);
+
+        return response()->json(['message' => 'Suara berhasil disimpan'], 200);
+    } catch (\Exception $e) {
+        return response()->json(['message' => $e->getMessage()], 500);
     }
-
-    return response()->json(['requestData' => $request->all()], 200);
 }
+
+
+
 
 
 
